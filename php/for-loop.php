c9bc973a62d924a2fe86aca2e061a03e83e44271
objectDisorientedProgrammer <tikiman92@gmail.com>
2024-09-30 16:33:25 -0700

    adding PHP reference files

---
+++ php/for-loop.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <title>Leap Years</title>
+  </head>
+  <body>
+    <?php
+      for ($leap = 2004; $leap < 2050; $leap = $leap + 4) {
+        echo "<p>$leap</p>";
+      }
+	  
+	  // for each loop
+	  $langs = array("JavaScript",
+          "HTML/CSS", "PHP",
+          "Python", "Ruby");
+	  
+	  foreach ($langs as $lang) {
+              echo "<li>$lang</li>";
+          }
+		unset($lang);
+    ?>
+  </body>
+</html>
---
+++ php/if-conditional.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $name="some name";
+		
+		if($name == "me")
+		{
+			echo "hi me";
+		}
+		elseif($name == "some name")
+		{
+			echo "Welcome";
+		}
+		else
+		{
+			echo "not me";
+		}
+      ?>
+    </p>
+  </body>
+</html>
