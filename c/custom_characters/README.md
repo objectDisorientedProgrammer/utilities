9120c74e07fc0329557d0ca6df601f95be8e20d7
objectDisorientedProgrammer <tikiman92@gmail.com>
2025-03-27 20:42:33 -0700

    implemented crude command line arg interpreter

--- c/custom_characters/README.md
+++ c/custom_characters/README.md
@@ -1,5 +1,40 @@
-# Custom Character String Generator 
-This code is used to print strings to stdout which use custom defined characters.
+# Custom Character String Generator
+Transcribe strings with custom defined character sets.
 
-Use `config.h` to define character attributes.
+### Character Definition
+Custom character sets are defined in comma separated values files (.csv).
+Character sets *must* define ASCII space ( , decimal 32) through backtick (\`, decimal 96).
+Note that this excludes lower case letters a-z, |, {, }, and ~.
 
+The first line defines character width (in characters), character height (in characters), foreground character, and background character.
+
+The remaining lines define each representation for each required ASCII value (0-9, A-Z, etc.). An `f` is forground, and a `b` is background.
+
+#### Example
+This will define 5x5 characters: ' ' (space), '0', and '1'
+
+    5,5,@, 
+     ,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b
+    0,b,f,f,f,b,f,b,b,b,f,f,b,b,b,f,f,b,b,b,f,b,f,f,f,b
+    1,b,b,f,b,b,b,f,f,b,b,b,b,f,b,b,b,b,f,b,b,f,f,f,f,f
+
+See `encoding_example.csv` for more details.
+
+### Running
+`./transcribe encoding.csv text to be generated`
+
+#### Example
+Running `./transcribe encoding_example.csv hello 2025` will output:
+
+    @   @ @@@@@ @     @     @@@@@      @@@   @@@   @@@  @@@@@
+    @   @ @     @     @     @   @     @   @ @   @ @   @ @    
+    @@@@@ @@@   @     @     @   @        @  @   @    @  @@@@ 
+    @   @ @     @     @     @   @       @   @   @   @       @
+    @   @ @@@@@ @@@@@ @@@@@ @@@@@     @@@@@  @@@  @@@@@ @@@@ 
+
+### Building
+This project uses SCons build system. Run `scons` from the directory to build and `scons -c` to clean.
+
+
+### ToDo
+[ ] output to file
--- c/custom_characters/test.c
+++ c/custom_characters/test.c
@@ -16,11 +16,198 @@
 
 #include "characters.h"
 #include "config.h"
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct metadata
+{
+    int width;
+    int height;
+    char fill_char;
+    char bg_char;
+} metadata_t;
+
+metadata_t fileinfo;
+#define CHARMAP_SIZE 128
+char* charmap[CHARMAP_SIZE];
+
+void read_csv(const char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Failed to open file");
+        return;
+    }
+
+    char line[1024];
+
+    // read the width, height, foreground, and background character
+    if (fgets(line, sizeof(line), file))
+    {
+        char *token = strtok(line, ",");
+        if (token != NULL)
+            fileinfo.width = atoi(token);
+        token = strtok(NULL, ",");
+        if (token != NULL)
+            fileinfo.height = atoi(token);
+        token = strtok(NULL, ",");
+        if (token != NULL)
+            fileinfo.fill_char = *token;
+        token = strtok(NULL, ",");
+        if (token != NULL)
+            fileinfo.bg_char = *token;
+        token = strtok(NULL, ",");
+        while (token != NULL)
+            token = strtok(NULL, ",");
+    }
+
+    // Read each line in the CSV file
+    while (fgets(line, sizeof(line), file))
+    {
+        // Tokenize the line by comma
+        char *token = strtok(line, ",");
+        while (token != NULL)
+        {
+            //if (isdigit(*token))
+            {
+                char *digit=token;
+                int end = fileinfo.width*fileinfo.height+1;
+                char *encoding = (char*) malloc(end * sizeof(char));
+                if (encoding == NULL)
+                {
+                    perror("failed to allocate character encoding memory");
+                    return;
+                }
+                for (int i = 0; i < end-1; ++i)
+                {
+                    token = strtok(NULL, ",");
+                    if (token != NULL)
+                    {
+                        //printf("%s ", token);
+                        encoding[i] = *token;
+                    }
+                }
+                encoding[end]='\0';
+                charmap[*digit] = encoding;
+            }
+            token = strtok(NULL, ",");
+        }
+        //printf("\n");
+    }
+
+    fclose(file);
+}
+
+void printFullChar(char c)
+{
+    for (int i = 0; charmap[c][i] != '\0'; ++i)
+    {
+        if (i != 0 && i % fileinfo.width == 0)
+            printf("\n");
+        if (charmap[c][i] == 'b')
+        {
+            printf("%c", fileinfo.bg_char);
+        }
+        else if (charmap[c][i] == 'f')
+        {
+            printf("%c", fileinfo.fill_char);
+        }
+    }
+    puts("");
+}
+
+void printCharacters(char* word)
+{
+    puts("");
+    for (int c=0; word[c]; ++c)
+        printFullChar(toupper(word[c]));
+    puts("");
+}
+
+int bufferChar(char c, int startIndex, char *buf, int buf_size, int row)
+{
+    char **table = charmap;
+    if (startIndex + fileinfo.width < buf_size && table[c] != NULL)
+    {
+        // if the character is space and not the first character in the string
+        // go back on index in the buffer since each normal character ends
+        // with an extra space appended
+        if (c == ' ' && startIndex > 0)
+            --startIndex;
+        int i;
+        for (i = 0; i < fileinfo.width; ++i)
+        {
+            buf[startIndex] = table[c][row * (fileinfo.width) + i] == 'b' ? fileinfo.bg_char : fileinfo.fill_char;
+            ++startIndex;
+        }
+        // if the character is not space, add padding between characters
+        if (c != ' ')
+        {
+            buf[startIndex] = fileinfo.bg_char;
+            ++startIndex;
+        }
+    }
+    return startIndex;
+}
+
+void printString(char* str)
+{
+    int len = strlen(str);
+    
+    // create space to store the Nth row of all characters in the string plus space and \0
+    int linebufferSize = len * (fileinfo.width+1) + 1;
+    char linebuffer[linebufferSize];
+    int bufferIndex = 0;
+
+    puts("");
+    for (int row = 0; row < fileinfo.height; ++row)
+    {
+        bufferIndex = 0;
+        // buffer each character's encoding
+        for (int c=0; str[c]; ++c)
+        {
+            bufferIndex = bufferChar(toupper(str[c]), bufferIndex, linebuffer, linebufferSize, row);
+        }
+        // send buffer to stdout
+        linebuffer[bufferIndex-1] = '\0';
+        printf("%s\n", linebuffer);
+    }
+}
+
+void releaseMemory(char **map, int len)
+{
+    for (char i = '0'; i <= 'Z'; ++i)
+    {
+        if (map[i] != NULL)
+        {
+            //printf("freeing { %c :'%s'}\n", i, charmap[i]);
+            free(map[i]);
+        }
+        //else
+        //    printf("%c not defined\n", i);
+    }
+}
+
+void read_encoding_from_csv(const char *filename)
+{
+    read_csv(filename);
+    
+    #if 0 // for testing
+    read_csv("encoding_example.csv");
+    printString("0987654321");
+    printString("abcdefghij");
+    printString("klmnopqrst");
+    printString("uvwxyz");
+    releaseMemory(charmap, CHARMAP_SIZE);
+    puts("");
+    #endif
+}
 
 int main(int argc, char *argv[])
 {
-    char buf[1024];
+    /*char buf[1024];
 
     for (char i = '0'; i <= '9'; ++i)
     {
@@ -61,6 +248,44 @@ int main(int argc, char *argv[])
     // add null terminator to string at last index
     bigString[next] = '\0';
     printf("%s", bigString);
+    */
+    char enormousBuffer[8192];
+    char filename[1024];
+    
+
+    // process command line args
+    if (argc > 1)
+    {
+        int a = 1;
+        printf("filename = '%s'\n", argv[a]);
+        strncpy(filename, argv[a], 1023);
+        ++a;
+        
+        while (argv[a])
+        {
+            strncat(enormousBuffer, argv[a], 50);
+            ++a;
+            strncat(enormousBuffer, " ", 2);
+        }
+        enormousBuffer[strlen(enormousBuffer)-1] = '\0';
+        printf("userStr= '%s'\n", enormousBuffer);
+
+    }
+    else
+    {
+        // default values
+        strcpy(filename, "encoding_example.csv"); // default file
+        strcpy(enormousBuffer, "March 27th 2025"); // default string
+    }
+
+    // initialize character map
+    memset(charmap, 0, CHARMAP_SIZE);
+
+    read_encoding_from_csv(filename);
+
+    printString(enormousBuffer);
+
+    releaseMemory(charmap, CHARMAP_SIZE);
 
     return 0;
 }
